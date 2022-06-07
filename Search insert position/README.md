cdc0ad61b5dd967ba8c1e6889e9477f51aca6a14
Axel Muñoz <axel.munoz@picnic.pe>
2022-06-06 23:16:10 -0500

    update readme solution

---
+++ Search insert position/README.md
@@ -0,0 +1,32 @@
+# Search Insert Position
+
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+>You must write an algorithm with ```O(log n)``` runtime complexity.
+
+---
+
+## Example 1:
+```javascript
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+```
+
+## Example 2:
+```javascript
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+```
+## Example 3:
+```javascript
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+```
+---
+
+# **_Solution_**:
+![this is an image](./test.png)
+
+![this is an image](./solution.png)
+
+
--- Search insert position/solution.test.js
+++ Search insert position/solution.test.js
@@ -1,11 +1,16 @@
 const {searchIndex} = require('./solution');
 
-test('return the position of the array where stay the number 2', () => {
-  let input = [1,2,3,4,5]
+test('return the position of the array where the number is 5', () => {
+  let input = [1,3,5,6]
+  expect(searchIndex(input, 5)).toBe(2)
+})
+
+test('return the position of the array sorted if the number 2 exist', () => {
+  let input = [1,3,5,6]
   expect(searchIndex(input, 2)).toBe(1)
 })
 
-test('return the position of the number in the array if add this number in the array and then the array is sorted', () => {
-  let input = [1,2,3,4,5]
-  expect(searchIndex(input, 6)).toBe(5)
+test('return the position of the array sorted if the number 7 exist', () => {
+  let input = [1,3,5,6]
+  expect(searchIndex(input, 7)).toBe(4)
 })
